ee67be02360c00d0e18284ecc7296fa904a4a832
Vishal Kumar <vishal.k0905@gmail.com>
2020-08-08 22:44:24 +0530

    Updates

---
+++ precache-manifest.be85e7526bde2045dd8c204294ba5d3c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "579dd7827847fea225377f34af85a0c8",
+    "url": "/friends-avatar/index.html"
+  },
+  {
+    "revision": "e2fdaa37a809b1fa7fbf",
+    "url": "/friends-avatar/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "76261ed4091daf76d61f",
+    "url": "/friends-avatar/static/css/main.a8c44b1b.chunk.css"
+  },
+  {
+    "revision": "e2fdaa37a809b1fa7fbf",
+    "url": "/friends-avatar/static/js/2.ddaa2ce9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/friends-avatar/static/js/2.ddaa2ce9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76261ed4091daf76d61f",
+    "url": "/friends-avatar/static/js/main.81ad8c22.chunk.js"
+  },
+  {
+    "revision": "3fff816e65e1cb44502f",
+    "url": "/friends-avatar/static/js/runtime-main.b9074794.js"
+  },
+  {
+    "revision": "1c7334345a70477ef055696837ff0cd4",
+    "url": "/friends-avatar/static/media/SEGA.1c733434.woff"
+  }
+]);
